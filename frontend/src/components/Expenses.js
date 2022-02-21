888d083f6a029915b597ecb7edb46600ac51b4fc
ankur-srivastava <ankurlkw@gmail.com>
2022-02-21 07:56:03 +0530

    Assignment Solution

--- frontend/src/components/Expenses.js
+++ frontend/src/components/Expenses.js
@@ -1,9 +1,11 @@
 import ExpenseItem from "./ExpenseItem"
 
 function Expenses({expenses}) {
-    return expenses.map((expense)=>(
-        <ExpenseItem title={expense.title} date={expense.date} amount={expense.amount}></ExpenseItem>    
-    ))
+    return <div className="expenses">
+        {expenses.map((expense)=>(
+            <ExpenseItem title={expense.title} date={expense.date} amount={expense.amount}></ExpenseItem>    
+        ))}
+    </div>
 }
 
 export default Expenses
