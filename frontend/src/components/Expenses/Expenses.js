71f39321a373bca103e47f9df1527cd99b37e446
ankur-srivastava <ankurlkw@gmail.com>
2022-02-22 08:02:37 +0530

    Minor changes

--- frontend/src/components/Expenses/Expenses.js
+++ frontend/src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import ExpenseItem from "./ExpenseItem"
 
-function Expenses({expenses}) {
+const Expenses = ({expenses}) => {
     return <div className="expenses">
         {expenses.map((expense)=>(
             <ExpenseItem title={expense.title} date={expense.date} amount={expense.amount}></ExpenseItem>    
